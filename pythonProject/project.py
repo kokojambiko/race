43a8af7c9ae991b6227eadde8c2bc7adbba42660
nur <ismailiw159@gmail.com>
2024-02-03 16:34:28 +0300

    ola

--- pythonProject/project.py
+++ pythonProject/project.py
@@ -25,6 +25,8 @@ menu_back=pygame.image.load("menu_back.png")
 menu_back=pygame.transform.scale(menu_back,(840,750))
 background = pygame.image.load("background.png")
 background=pygame.transform.scale(background,(840,750))
+background1 = pygame.image.load("background.png")
+background1=pygame.transform.scale(background1,(840,750))
 blue_car=pygame.image.load("blue_car.png")
 blue_car = pygame.transform.scale(blue_car, (70, 100))
 red_car=pygame.image.load("red_car.png")
@@ -35,15 +37,15 @@ main_car=pygame.image.load("car.png")
 main_car = pygame.transform.scale(main_car, (70, 100))
 
 cool_car_up=pygame.image.load("cool_car_up.png")
-cool_car_up= pygame.transform.scale(cool_car_up, (70, 100))
+cool_car_up= pygame.transform.scale(cool_car_up, (75, 100))
 blue_car_up=pygame.image.load("blue_car_up.png")
-blue_car_up = pygame.transform.scale(blue_car_up, (70, 100))
+blue_car_up = pygame.transform.scale(blue_car_up, (74, 100))
 red_car_up=pygame.image.load("red_car_up.png")
-red_car_up = pygame.transform.scale(red_car_up, (70, 100))
+red_car_up = pygame.transform.scale(red_car_up, (73, 100))
 green_car_up=pygame.image.load("green_car_up.png")
-green_car_up = pygame.transform.scale(green_car_up, (70, 100))
+green_car_up = pygame.transform.scale(green_car_up, (72, 100))
 main_car=pygame.image.load("car.png")
-main_car = pygame.transform.scale(main_car, (70, 100))
+main_car = pygame.transform.scale(main_car, (71, 100))
 mon_car=pygame.image.load("mon_car_up.png")
 mon_car = pygame.transform.scale(mon_car, (70, 100))
 
@@ -84,15 +86,18 @@ clock = pygame.time.Clock()
 
 label = pygame.font.SysFont('Verdana', 24)
 label1 = pygame.font.SysFont('arial', 24)
-play_label = label.render('Играть!', False, (0, 0, 0))
-play_label_rect = play_label.get_rect(topleft=(370,250))
+play_label = pygame.image.load("play_button.png")
+play_label = pygame.transform.scale(play_label, (120, 90))
+
+play_label_rect = play_label.get_rect(topleft=(360,250))
 restart_label = label1.render('Играть заново!', False, (255, 255, 255))
 restart_label_rect = restart_label.get_rect(topleft=(300, 450))
-skin_label = label1.render('Скинчики', False, (0, 0, 0))
-skin_label_rect = skin_label.get_rect(topleft=(360, 350))
+skin_label = pygame.image.load("skin_button.png")
+skin_label= pygame.transform.scale(skin_label, (120, 90))
+skin_label_rect = skin_label.get_rect(topleft=(360, 400))
 ekip_label=label1.render('Экипирован', False, (0, 0, 0))
 menu_label=label1.render('Меню', False, (255, 255, 255))
-menu_label_rect = menu_label.get_rect(topleft=(350, 550))
+menu_label_rect = menu_label.get_rect(topleft=(450, 550))
 
 
 
@@ -129,6 +134,7 @@ cr_y = 440
 cr_speed = 10
 
 
+
 mon_x = random.choice(cr_rand)
 mon_y = -100
 mon_x2 = random.choice(cr_rand2)
@@ -145,25 +151,42 @@ main=True
 car_false=[cool,red,green,blue,mon,main]
 
 cr_cnt=0
-iv=False
-menu=1
-gameplay = 0
+skin=False
+lose=False
+menu=True
+gameplay =False
 running = True
 spd =2
 spd2= 7
-
-
-
+spdcr1=random.randrange(5,20)
+spdcr2=random.randrange(5,20)
+spdcr3=random.randrange(5,20)
+spdcr4=random.randrange(5,20)
 car_img=main_car
+
+
+
 while running:
+    if main == True:
+        car_img =  main_car
+    if cool == True:
+        car_img = cool_car_up
+    if mon == True:
+        car_img = mon_car
+    if red == True:
+        car_img = red_car_up
+    if green == True:
+        car_img = green_car_up
+    if blue == True:
+        car_img = blue_car_up
 
     car_rect = car_img.get_rect(topleft=(cr_x, cr_y))
     mon_rect = mon_img.get_rect(topleft=(mon_x, mon_y))
     mon_rect2 = mon_img2.get_rect(topleft=(mon_x2, mon_y2))
     mon_rect3= mon_img3.get_rect(topleft=(mon_x3, mon_y3))
-    if menu==1:
-        # waiting_menu.play()
-        screen.blit(background,(0,0))
+    if menu:
+        waiting_menu.play()
+        screen.blit(background1,(0,0))
         screen.blit(car1,(crx1,cry1))
         screen.blit(car2, (crx2,cry2))
         screen.blit(car3, (crx3, cry3))
@@ -171,22 +194,26 @@ while running:
         screen.blit(play_label,(play_label_rect))
         screen.blit(skin_label,(skin_label_rect))
         clock.tick(FPS)
-        cry1 +=5
-        cry2 += 7
-        cry3 += 10
-        cry4 += 8
+        cry1 +=spdcr1
+        cry2 += spdcr2
+        cry3 += spdcr3
+        cry4 += spdcr4
         if cry1 >750:
             cry1=-50
             car1 = random.choice(cars)
+            spdcr1 = random.randrange(5, 15)
         if cry2 >750:
             cry2=-200
             car2 = random.choice(cars)
+            spdcr2 = random.randrange(5, 15)
         if cry3 >750:
             cry3=-100
             car3 = random.choice(cars)
+            spdcr3 = random.randrange(5, 15)
         if cry4 >750:
             cry4=-150
             car4 = random.choice(cars)
+            spdcr4 = random.randrange(5, 15)
         # Ввод процесса (события)
         for event in pygame.event.get():
             # check for closing window
@@ -195,20 +222,32 @@ while running:
             mouse=pygame.mouse.get_pos()
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if play_label_rect.collidepoint(event.pos):
-                    cr_y = 440
-                    cr_x=random.choice(cr_rand)
-                    mon_y=-100
-                    mon_x=random.choice(cr_rand)
-                    menu = 0
-                    gameplay=1
+
+                    if lose==False:
+                        gameplay=True
+                        print(gameplay)
+                        cr_y = 440
+                        cr_x=random.choice(cr_rand)
+                        mon_y=-200
+                        cnt =0
+                        mon_y2 = -200
+                        mon_y3 = -200
+                        mon_x=random.choice(cr_rand)
+                        menu=False
+
+
 
                     waiting_menu.stop()
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if skin_label_rect.collidepoint(event.pos):
-                    menu=2
+                    gameplay=False
+                    lose=False
+                    skin=True
+                    menu=False
         pygame.display.flip()
-    if menu == 2:
-        screen.blit(background,(0,bg_y))
+    if skin:
+        lose = False
+        screen.blit(background1,(0,0))
         screen.blit(cars2[skin_cnt],(385,300))
         screen.blit(arrow_right,(arrow_right_rect))
         screen.blit(arrow_left,(arrow_left_rect))
@@ -231,76 +270,72 @@ while running:
                         skin_cnt-=1
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if x_rect.collidepoint(event.pos):
-                    menu=1
+                    menu=True
+                    skin=False
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if ok_rect.collidepoint(event.pos):
-                    if skin_cnt==5:
+                    if skin_cnt==5 and mon==False:
                         for i in car_false:
                             if i == True:
                                 i = False
-                        mon = True
-                        print(car_false)
-                        print(car_img)
-                        screen.blit(ekip_label,(350,450))
-                    if skin_cnt==4:
+                            mon = True
+                        screen.blit(ekip_label, (350, 450))
+                    if skin_cnt==4 and cool==False:
                         for i in car_false:
                             if i == True:
                                 i = False
-                        cool = True
-                        screen.blit(ekip_label,(350,450))
-                    if skin_cnt==3:
+                            cool = True
+                        screen.blit(ekip_label, (350, 450))
+                    if skin_cnt==3 and green==False:
                         for i in car_false:
                             if i == True:
                                 i = False
                         green = True
-                        screen.blit(ekip_label,(350,450))
-                    if skin_cnt==2:
+                        screen.blit(ekip_label, (350, 450))
+                    if skin_cnt==2 and red==False:
                         for i in car_false:
                             if i == True:
                                 i = False
                         red = True
-                        screen.blit(ekip_label,(350,450))
-                    if skin_cnt==1:
+                        screen.blit(ekip_label, (350, 450))
+                    if skin_cnt==1 and blue==False:
                         for i in car_false:
                             if i == True:
                                 i = False
                         blue = True
-                        screen.blit(ekip_label,(350,450))
-                    if skin_cnt==0:
+                        screen.blit(ekip_label, (350, 450))
+                    if skin_cnt==0 and main==False:
                         for i in car_false:
                             if i == True:
                                 i = False
                         main = True
-                        screen.blit(ekip_label,(350,450))
-            if main==True:
-                car_img=main_car
-            if cool==True:
-                car_img=cool_car_up
-            if mon==True:
-                car_img=mon_car
-            if red == True:
-                car_img = red_car_up
-            if green == True:
-                car_img=green_car_up
-            if blue == True:
-                car_img=blue_car_up
+                        screen.blit(ekip_label, (350, 450))
+                pygame.display.flip()
+            # if main==True:
+            #     car_img=main_car
+            # if cool==True:
+            #     car_img=cool_car_up
+            # if mon==True:
+            #     car_img=mon_car
+            # if red == True:
+            #     car_img = red_car_up
+            # if green == True:
+            #     car_img=green_car_up
+            # if blue == True:
+            #     car_img=blue_car_up
         pygame.display.flip()
 
 
 
-    if gameplay== 1:
+    if gameplay:
+        print(car_img)
+        print(mon)
         screen.blit(background, (0, bg_y))
         screen.blit(background, (0, bg_y - 750))
         screen.blit(car_img,(cr_x,cr_y))
         screen.blit(mon_img, (mon_x, mon_y))
-
-
-
-
         bg_y += spd
         mon_y += spd2
-
-
         if cnt > 0:
             mon_y2 += spd2
             screen.blit(mon_img2 ,(mon_x2, mon_y2))
@@ -356,11 +391,18 @@ while running:
         value = label.render("Ваш счёт: " + str(cnt), True, (0, 0, 0))
         screen.blit(value,(20,20))
         if car_rect.colliderect(mon_rect) or car_rect.colliderect(mon_rect2) or car_rect.colliderect(mon_rect3):
-            gameplay = 2
+            cr_y = 440
+            mon_y= -100
+            mon_y2= -100
+            mon_y3 = -100
+            gameplay=False
+            lose=True
+
         pygame.display.flip()
 
 
-    elif gameplay == 2:
+    if lose:
+
         # screen.fill((87, 88, 89))
         # screen.blit(lose_img, (280, 150))
         # screen.blit(restart_label, (restart_label_rect))
@@ -372,20 +414,25 @@ while running:
             mouse = pygame.mouse.get_pos()
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if restart_label_rect.collidepoint(event.pos):
+                    lose=False
                     cr_y = 440
                     cr_x = random.choice(cr_rand)
                     mon_y = -100
                     mon_y2 = -100
                     mon_y3 = -100
                     mon_x = random.choice(cr_rand)
-                    gameplay = 1
+
+                    gameplay=True
                     spd =2
                     spd2=7
                     cnt=0
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if menu_label_rect.collidepoint(event.pos):
-                    gameplay=0
-                    menu=1
+                    skin= False
+                    lose=False
+                    gameplay=False
+                    menu=True
+
         screen.fill((87, 88, 89))
         screen.blit(lose_img, (280, 150))
         screen.blit(restart_label, (restart_label_rect))
